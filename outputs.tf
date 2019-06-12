ccf79b36f097466ad262cae4ef09972d11db38ed
Ben Fitzhardinge <ben.fitzhardinge@ausvet.com.au>
2019-06-12 17:06:02 +0800

    output hasura_url

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,14 @@ output "hasura_alb_address" {
   value = "${aws_alb.hasura.dns_name}"
 }
 
+output "hasura_url" {
+  value = "${aws_route53_record.dns_record.dns_name}"
+}
+
 output "hasura_alb_id" {
   value = "${aws_alb.hasura.id}"
 }
 
 output "hasura_access_key" {
-    value = "${var.hasura_access_key}"
+  value = "${var.hasura_access_key}"
 }
